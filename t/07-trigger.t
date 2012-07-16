rsrchboy/moosex-attributeshortcuts

commit d11e67a3ad645848083dae850150e0397f84eb6a

remove conflicting 00-load.t

t/00-load.t
@@ -1,8 +1,0 @@
-#!/usr/bin/env perl
-
-use Test::More tests => 1;
-
-use Moose;
-BEGIN { use_ok 'MooseX::AttributeShortcuts' }
-
-diag("Testing MooseX-AttributeShortcuts $MooseX::AttributeShortcuts::VERSION, Perl $], $^X");

commit 1cc9a70f439478c266a5292ed0c7dfda46ee51d9

revert t/01-basic.t breakage from d11e67a
*sigh*

t/01-basic.t
@@ -1,8 +1,6 @@
 use strict;
 use warnings;
 
-my $trigger_called = 0;
-
 {
     package TestClass;
 
@@ -14,14 +12,6 @@ my $trigger_called = 0;
     has bar => (is => 'ro', builder => 1);
     has baz => (is => 'rwp', builder => 1);
 
-    sub _build_bar {}
-    sub _build_baz {}
-
-    has fee => (is => 'rw', trigger => 1);
-    sub _trigger_fee { $trigger_called++ }
-
-    has _foe => (is => 'rw', trigger => 1);
-    sub _trigger__foe { $trigger_called++ }
 }
 
 use Test::More;
@@ -29,26 +19,7 @@ use Test::Moose;
 
 require 't/funcs.pm' unless eval { require funcs };
 
-with_immutable {
-
-    test_class('TestClass');
-
-    for my $name (qw{ fee _foe }) {
-
-        $trigger_called = 0;
-
-        my $a = TestClass->meta->get_attribute($name);
-        ok $a->has_trigger, "$name has a trigger";
-
-        ok !$trigger_called, 'no trigger calls yet';
-        my $tc = TestClass->new($name => 'Ian');
-        is $trigger_called, 1, 'trigger called once';
-        $tc->$name('Cormac');
-        is $trigger_called, 2, 'trigger called again';
-
-    }
-
-} 'TestClass';
+test_class('TestClass');
 
 done_testing;
 

t/07-trigger.t
@@ -1,0 +1,54 @@
+use strict;
+use warnings;
+
+my $trigger_called = 0;
+
+{
+    package TestClass;
+
+    use Moose;
+    use namespace::autoclean;
+    use MooseX::AttributeShortcuts;
+
+    has foo => (is => 'rwp');
+    has bar => (is => 'ro', builder => 1);
+    has baz => (is => 'rwp', builder => 1);
+
+    sub _build_bar {}
+    sub _build_baz {}
+
+    has fee => (is => 'rw', trigger => 1);
+    sub _trigger_fee { $trigger_called++ }
+
+    has _foe => (is => 'rw', trigger => 1);
+    sub _trigger__foe { $trigger_called++ }
+}
+
+use Test::More;
+use Test::Moose;
+
+require 't/funcs.pm' unless eval { require funcs };
+
+with_immutable {
+
+    test_class('TestClass');
+
+    for my $name (qw{ fee _foe }) {
+
+        $trigger_called = 0;
+
+        my $a = TestClass->meta->get_attribute($name);
+        ok $a->has_trigger, "$name has a trigger";
+
+        ok !$trigger_called, 'no trigger calls yet';
+        my $tc = TestClass->new($name => 'Ian');
+        is $trigger_called, 1, 'trigger called once';
+        $tc->$name('Cormac');
+        is $trigger_called, 2, 'trigger called again';
+
+    }
+
+} 'TestClass';
+
+done_testing;
+