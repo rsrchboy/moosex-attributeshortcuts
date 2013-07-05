d1fb7ef75b696394b53a80f2db97e3b16405d3f6
Olaf Alders <olaf@wundersolutions.com>
2013-07-05 12:29:20 -0400

    Fixes typo in predicate documentation.

--- lib/MooseX/AttributeShortcuts.pm
+++ lib/MooseX/AttributeShortcuts.pm
@@ -446,7 +446,7 @@ Specifying C<predicate =E<gt> 1> will cause the following options to be set:
 
 or, if your attribute name begins with an underscore:
 
-    predicate => "_has$name"
+    predicate => "_has_$name"
 
 (that is, an attribute named "_foo" would get "_has_foo")
 
