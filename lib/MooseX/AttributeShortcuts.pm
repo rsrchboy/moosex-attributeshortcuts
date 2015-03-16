615d735e47ed18fda612a169f02419cc12e8ebe9
Chris Weyl <cweyl@alumni.drew.edu>
2015-03-15 21:32:30 -0700

    Break out $_process_options into smaller methods
    ...because that was getting kinda ridiculous.

--- lib/MooseX/AttributeShortcuts.pm
+++ lib/MooseX/AttributeShortcuts.pm
@@ -81,131 +81,20 @@ use Moose::Util::TypeConstraints;
             my $_opt = sub { $_has->(@_) ? $options->{$_[0]} : q{} };
             my $_ref = sub { ref $_opt->(@_) || q{}                };
 
-            if ($options->{is}) {
+            # handle: is => ... 
+            $class->_mxas_is_rwp($name, $options, $_has, $_opt, $_ref);
+            $class->_mxas_is_lazy($name, $options, $_has, $_opt, $_ref);
 
-                # handle: is => 'rwp'
-                if ($options->{is} eq 'rwp') {
+            # handle: isa_class, isa_role, isa_enum
+            $class->_mxas_isa_naughty($name, $options, $_has, $_opt, $_ref);
 
-                    $options->{is}     = 'ro';
-                    $options->{writer} = "$wprefix$name";
-                }
+            # handle: isa_instance_of => ... 
+            $class->_mxas_isa_instance_of($name, $options, $_has, $_opt, $_ref);
 
-                # handle: is => 'lazy'
-                if ($options->{is} eq 'lazy') {
+            # handle: constraint => ... 
+            $class->_mxas_constraint($name, $options, $_has, $_opt, $_ref);
+            $class->_mxas_coerce($name, $options, $_has, $_opt, $_ref);
 
-                    $options->{is}       = 'ro';
-                    $options->{lazy}     = 1;
-                    $options->{builder}  = 1
-                        unless $_has->('builder') || $_has->('default');
-                }
-            }
-
-            if (any { exists $options->{$_} } (qw{ isa_class isa_role isa_enum })) {
-
-                # (more than) fair warning...
-                deprecated(
-                    feature => 'undocumented-isa-constraints',
-                    message => 'Naughty! isa_class, isa_role, and isa_enum will be removed on or after 01 July 2015!',
-                );
-
-                # XXX undocumented -- not sure this is a great idea
-                $options->{isa} = class_type(delete $options->{isa_class})
-                    if $_has->('isa_class');
-                $options->{isa} = role_type(delete $options->{isa_role})
-                    if $_has->('isa_role');
-                $options->{isa} = enum(delete $options->{isa_enum})
-                    if $_has->('isa_enum');
-            }
-
-            # aka: isa => class_type(...)
-            if ($_has->('isa_instance_of')) {
-
-                if ($_has->('isa')) {
-
-                    $class->throw_error(
-                        q{Cannot use 'isa_instance_of' and 'isa' together for attribute }
-                        . $_opt->('definition_context')->{package} . '::' . $name
-                    );
-                }
-
-                $options->{isa} = class_type(delete $options->{isa_instance_of});
-            }
-
-            ### the pretty business of on-the-fly subtyping...
-            my $our_type;
-
-            if ($_has->('constraint')) {
-
-                # check for errors...
-                $class->throw_error('You must specify an "isa" when declaring a "constraint"')
-                    if !$_has->('isa');
-                $class->throw_error('"constraint" must be a CODE reference')
-                    if $_ref->('constraint') ne 'CODE';
-
-                # constraint checking! XXX message, etc?
-                push my @opts, constraint => $_opt->('constraint')
-                    if $_ref->('constraint') eq 'CODE';
-
-                # stash our original option away and construct our new one
-                my $isa     = $options->{original_isa} = $_opt->('isa');
-                $our_type ||= _acquire_isa_tc($isa)->create_child_type(@opts);
-            }
-
-            # "fix" the case of the hashref....  *sigh*
-            if ($_ref->('coerce') eq 'HASH') {
-
-                deprecated(
-                    feature => 'hashref-given-to-coerce',
-                    message => 'Passing a hashref to coerce is unsafe, and will be removed on or after 01 Jan 2015',
-                );
-
-                $options->{coerce} = [ %{ $options->{coerce} } ];
-            }
-
-            if ($_ref->('coerce') eq 'ARRAY') {
-
-                ### must be type => sub { ... } pairs...
-                my @coercions = @{ $_opt->('coerce') };
-                confess 'You must specify an "isa" when declaring "coercion"'
-                    unless $_has->('isa');
-                confess 'coercion array must be in pairs!'
-                    if @coercions % 2;
-                confess 'must define at least one coercion pair!'
-                    unless @coercions > 0;
-
-                my $our_coercion = Moose::Meta::TypeCoercion->new;
-                $our_type ||= _acquire_isa_tc($_opt->('isa'))->create_child_type;
-
-                $our_coercion->add_type_coercions(@coercions);
-                $our_type->coercion($our_coercion);
-                $options->{coerce} = 1;
-            }
-
-            if ($our_type && !$our_type->has_coercion) {
-
-                my $isa_type = _acquire_isa_tc($_opt->('isa'));
-
-                if ($isa_type->has_coercion && !$_ref->('coerce') && $_opt->('coerce') eq "1") {
-
-                    # create our coercion as a copy of the parent
-                    $our_type->coercion(Moose::Meta::TypeCoercion->new(
-                        type_constraint   => $our_type,
-                        type_coercion_map => [ @{ $isa_type->coercion->type_coercion_map } ],
-                    ));
-                }
-
-            }
-
-            # fin constraint mucking....
-            do { $options->{original_isa} = $_opt->('isa'); $options->{isa} = $our_type }
-                if $our_type;
-
-            if ($options->{lazy_build} && $options->{lazy_build} eq 'private') {
-
-                $options->{lazy_build} = 1;
-                $options->{clearer}    = "_clear_$name";
-                $options->{predicate}  = "_has_$name";
-            }
 
             my $is_private = sub { $name =~ /^_/ ? $_[0] : $_[1] };
             my $default_for = sub {
@@ -265,6 +154,173 @@ use Moose::Util::TypeConstraints;
             return $self->$orig($name, %options);
         };
 
+        # handle: is => 'rwp'
+        method _mxas_is_rwp => sub {
+            my ($class, $name, $options, $_has, $_opt, $_ref) = @_;
+
+            return unless $_opt->('is') eq 'rwp';
+
+            $options->{is}     = 'ro';
+            $options->{writer} = "$wprefix$name";
+
+            return;
+        };
+
+        # handle: is => 'lazy'
+        method _mxas_is_lazy => sub {
+            my ($class, $name, $options, $_has, $_opt, $_ref) = @_;
+
+            return unless $_opt->('is') eq 'lazy';
+
+            $options->{is}       = 'ro';
+            $options->{lazy}     = 1;
+            $options->{builder}  = 1
+                unless $_has->('builder') || $_has->('default');
+
+            return;
+        };
+
+        # handle: lazy_build => 'private'
+        method _mxas_lazy_build_private => sub {
+            my ($class, $name, $options, $_has, $_opt, $_ref) = @_;
+
+            return unless $_opt->('lazy_build') eq 'private';
+
+            $options->{lazy_build} = 1;
+            $options->{clearer}    = "_clear_$name";
+            $options->{predicate}  = "_has_$name";
+
+            return;
+        };
+
+        # handle: isa_class, isa_role, isa_enum
+        method _mxas_isa_naughty => sub {
+            my ($class, $name, $options, $_has, $_opt, $_ref) = @_;
+
+            return unless
+                any { exists $options->{$_} } qw{ isa_class isa_role isa_enum };
+
+            # (more than) fair warning...
+            deprecated(
+                feature => 'undocumented-isa-constraints',
+                message => 'Naughty! isa_class, isa_role, and isa_enum will be removed on or after 01 July 2015!',
+            );
+
+            # XXX undocumented -- not sure this is a great idea
+            $options->{isa} = class_type(delete $options->{isa_class})
+                if $_has->('isa_class');
+            $options->{isa} = role_type(delete $options->{isa_role})
+                if $_has->('isa_role');
+            $options->{isa} = enum(delete $options->{isa_enum})
+                if $_has->('isa_enum');
+
+            return;
+        };
+
+        # handle: isa_instance_of => ... 
+        method _mxas_isa_instance_of => sub {
+            my ($class, $name, $options, $_has, $_opt, $_ref) = @_;
+
+            return unless $_has->('isa_instance_of');
+
+            if ($_has->('isa')) {
+
+                $class->throw_error(
+                    q{Cannot use 'isa_instance_of' and 'isa' together for attribute }
+                    . $_opt->('definition_context')->{package} . '::' . $name
+                );
+            }
+
+            $options->{isa} = class_type(delete $options->{isa_instance_of});
+
+            return;
+        };
+
+        method _mxas_constraint => sub {
+            my ($class, $name, $options, $_has, $_opt, $_ref) = @_;
+
+            return unless $_has->('constraint');
+
+            # check for errors...
+            $class->throw_error('You must specify an "isa" when declaring a "constraint"')
+                if !$_has->('isa');
+            $class->throw_error('"constraint" must be a CODE reference')
+                if $_ref->('constraint') ne 'CODE';
+
+            # constraint checking! XXX message, etc?
+            push my @opts, constraint => $_opt->('constraint')
+                if $_ref->('constraint') eq 'CODE';
+
+            # stash our original option away and construct our new one
+            my $isa         = $options->{original_isa} = $_opt->('isa');
+            $options->{isa} = _acquire_isa_tc($isa)->create_child_type(@opts);
+
+            return;
+        };
+
+        method _mxas_coerce => sub {
+            my ($class, $name, $options, $_has, $_opt, $_ref) = @_;
+
+            # "fix" the case of the hashref....  *sigh*
+            if ($_ref->('coerce') eq 'HASH') {
+
+                deprecated(
+                    feature => 'hashref-given-to-coerce',
+                    message => 'Passing a hashref to coerce is unsafe, and will be removed on or after 01 Jan 2015',
+                );
+
+                $options->{coerce} = [ %{ $options->{coerce} } ];
+            }
+
+            if ($_ref->('coerce') eq 'ARRAY') {
+
+                ### must be type => sub { ... } pairs...
+                my @coercions = @{ $_opt->('coerce') };
+                confess 'You must specify an "isa" when declaring "coercion"'
+                    unless $_has->('isa');
+                confess 'coercion array must be in pairs!'
+                    if @coercions % 2;
+                confess 'must define at least one coercion pair!'
+                    unless @coercions > 0;
+
+                my $our_coercion = Moose::Meta::TypeCoercion->new;
+                my $our_type
+                    = $options->{original_isa}
+                    ? $options->{isa}
+                    : _acquire_isa_tc($_opt->('isa'))->create_child_type
+                    ;
+
+                $our_coercion->add_type_coercions(@coercions);
+                $our_type->coercion($our_coercion);
+
+                $options->{original_isa} ||= $options->{isa};
+                $options->{isa}            = $our_type;
+                $options->{coerce}         = 1;
+
+                return;
+            }
+
+            # If our original constraint has coercions and our created subtype
+            # did not have any (as specified in the 'coerce' option), then
+            # copy the parent's coercions over.
+
+            if ($_has->('original_isa') && $_opt->('coerce') eq '1') {
+
+                my $isa_type = _acquire_isa_tc($_opt->('original_isa'));
+
+                if ($isa_type->has_coercion) {
+
+                    # create our coercion as a copy of the parent
+                    $_opt->('isa')->coercion(Moose::Meta::TypeCoercion->new(
+                        type_constraint   => $_opt->('isa'),
+                        type_coercion_map => [ @{ $isa_type->coercion->type_coercion_map } ],
+                    ));
+                }
+
+            }
+
+            return;
+        };
 
         # we hijack attach_to_class in order to install our anon_builder, if
         # we have one.  Note that we don't go the normal
